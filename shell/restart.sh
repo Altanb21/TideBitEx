7eb907d8345da6d014b32f7aa75746844a7a926e
Luphia Chang <luphia@mermer.cc>
2022-08-02 19:24:00 +0800

    Update restart.sh

--- shell/restart.sh
+++ shell/restart.sh
@@ -1,3 +1,3 @@
 pm2 kill
-pm2 start /home/ubuntu/workspace/TideBitEx/bin/main.js
+cd /home/ubuntu/workspace/TideBitEx/ && pm2 start /home/ubuntu/workspace/TideBitEx/bin/main.js
 echo "I, [$(date +\%FT\%T.\%6N)] INFO -- : "restart… >> /home/ubuntu/workspace/TideBitEx/shell/restart.log
