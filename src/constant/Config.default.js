be44bd3d15af4401548afc0a8c8e30b8d4d2a0a9
TzuHanLiang <crocodilestear@gmail.com>
2022-08-09 14:27:15 +0800

    feat: update config

--- src/constant/Config.default.js
+++
@@ -1,7 +0,0 @@
-export const apiUrl = "";
-export const apiVersion = "";
-export const apiKey = '';
-export const apiSecret = '';
-export const wsProtocol =
-  window.location.protocol === "https:" ? "wss://" : "ws://";
-export const wsUrl = "";
--- src/constant/Config.js
+++ src/constant/Config.js
@@ -1,7 +1,6 @@
 export const apiUrl = "test.tidebit.network";
 export const apiVersion = "/api/v1";
-export const apiKey = '';
-export const apiSecret = '';
-export const wsProtocol =
-  window.location.protocol === "https:" ? "wss://" : "ws://";
+export const apiKey = "";
+export const apiSecret = "";
+export const wsProtocol = "wss://";
 export const wsUrl = "test.tidebit.network/ws";
