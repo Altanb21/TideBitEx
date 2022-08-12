35038d567162f53383a0d01c4effe4f955f7cbb6
TzuHanLiang <crocodilestear@gmail.com>
2022-08-12 14:29:32 +0800

    feat: fix infinite loop

--- src/pages/exchange.js
+++ src/pages/exchange.js
@@ -4,32 +4,22 @@ import DesktopExchange from "./desktop-exchange";
 import MobileExchange from "./mobile-exchange";
 import Layout from "../components/Layout";
 import StoreContext from "../store/store-context";
-import { useHistory, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Exchange = () => {
   const storeCtx = useContext(StoreContext);
   const location = useLocation();
-  const history = useHistory();
   const [isStart, setIsStart] = useState(false);
   const { width } = useViewport();
   const breakpoint = 428;
 
   useEffect(() => {
     if (location.pathname.includes("/markets")) {
-      if (!location.pathname.includes("/markets/")) {
-        history.push({
-          pathname: `/markets/ethhkd`,
-        });
-      }
-      let market = location.pathname.replace("/markets/", "");
       if (!isStart) {
         window.storeCtx = storeCtx;
-        storeCtx.start(market);
+        storeCtx.start();
         // storeCtx.sync();
         setIsStart(true);
-      }
-      if (isStart && storeCtx.selectedTicker?.market !== market) {
-        storeCtx.selectMarket(market);
       }
     }
     // ++TODO never called
@@ -37,7 +27,7 @@ const Exchange = () => {
       // storeCtx.stop();
       // clearInterval(interval)
     };
-  }, [history, isStart, location.pathname, storeCtx]);
+  }, [isStart, location.pathname, storeCtx]);
 
   return (
     <Layout>
