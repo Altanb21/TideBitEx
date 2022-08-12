ebe5c26055d42800212e46f6c4fb637805690b86
TzuHanLiang <crocodilestear@gmail.com>
2022-08-12 16:13:38 +0800

    feat: convert exponential numbers to decimal form on inpu

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -247,7 +247,7 @@ export const convertExponentialToDecimal = (exponentialNumber) => {
     // Unfortunately I can not return 1e-8 as 0.00000001, because even if I call parseFloat() on it,
     // it will still return the exponential representation
     // So I have to use .toFixed()
-    const result = exponentialNumber.toFixed(exponent);
+    const result = parseFloat(exponentialNumber).toFixed(exponent);
     return result;
   } else {
     return str;
