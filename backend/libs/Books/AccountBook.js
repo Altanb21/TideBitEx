dd72cca4fa078f21e2376c597897f6ebed15fbe3
TzuHanLiang <crocodilestear@gmail.com>
2022-08-26 20:17:29 +0800

    feat: add pricelist

--- backend/libs/Books/AccountBook.js
+++ backend/libs/Books/AccountBook.js
@@ -2,6 +2,15 @@ const BookBase = require("../BookBase");
 const SafeMath = require("../SafeMath");
 
 class AccountBook extends BookBase {
+  _ratio = {
+    hkd: 1,
+    twd: 0.2623,
+    jpy: 0.0715,
+    krw: 0.0073,
+    usd: 7.8,
+    eur: 8.9341,
+    try: 1.4637,
+  };
   constructor({ logger, markets }) {
     super({ logger, markets });
     this._config = { remove: false, add: false, update: true };
@@ -30,15 +39,25 @@ class AccountBook extends BookBase {
     if (!this._snapshot[memberId]) return null;
     else {
       if (instId)
-        return instId
-          .split("-")
-          .map((currency) => ({
-            ...this._snapshot[memberId][currency],
-            exchangeRate: this.priceList[currency.toLowerCase()],
-          }));
+        return instId.split("-").map((currency) => ({
+          ...this._snapshot[memberId][currency],
+          exchangeRate:
+            currency.toLowerCase === "try"
+              ? SafeMath.mult(
+                  SafeMath.div(this._ratio.hkd, this._ratio.try),
+                  this._ratio.usd
+                )
+              : this.priceList[currency.toLowerCase()] || 0,
+        }));
       return Object.values(this._snapshot[memberId]).map((account) => ({
         ...account,
-        exchangeRate: this.priceList[account.currency.toLowerCase()],
+        exchangeRate:
+          account.currency.toLowerCase === "try"
+            ? SafeMath.mult(
+                SafeMath.div(this._ratio.hkd, this._ratio.try),
+                this._ratio.usd
+              )
+            : this.priceList[account.currency.toLowerCase()] || 0,
       }));
     }
   }
