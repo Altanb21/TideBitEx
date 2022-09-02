a48e89b8abf11c8352deb80c486ada0ed9a8dabd
TzuHanLiang <crocodilestear@gmail.com>
2022-09-02 13:20:01 +0800

    feat: #814 test

--- dbmigrations/20220902001.sql
+++ dbmigrations/20220902001.sql
@@ -2,5 +2,5 @@ ALTER TABLE `outer_trades`
 ADD COLUMN (
         `trade_id` int(11) DEFAULT NULL,
         `voucher_id` int(11) DEFAULT NULL,
-        `create_at` datetime DEFAULT NULL,
+        `create_at` datetime DEFAULT NULL
     );
