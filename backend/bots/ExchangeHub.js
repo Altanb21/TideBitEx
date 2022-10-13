ab93e9ff9a596b27f7f5fcaba414404ba5582e0c
TzuHanLiang <crocodilestear@gmail.com>
2022-10-13 21:23:36 +0800

    feat: add comment

--- backend/bots/ExchangeHub.js
+++ backend/bots/ExchangeHub.js
@@ -3760,6 +3760,7 @@ class ExchangeHub extends Bot {
       // 1.1.可以從 data 解析出 orderId 及 memberId
       // 1.2.可以根據 orderId 從 database 取得 dbOrder
       // 1.3. dbOrder.member_id 同 data 解析出的 memberId
+      // 3. 判斷收到的資料對應的 order是否需要更新
       // 1.4 dbOrder.state 不為 0
       // 1.5 OKx api 回傳的 orderDetail state 不為 cancel
       market = this.tickersSettings[data.instId.toLowerCase().replace("-", "")];
