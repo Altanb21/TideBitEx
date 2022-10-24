85cb6c7bca7d72c6b8d362dc2ad4fdff3543c250
TzuHanLiang <crocodilestear@gmail.com>
2022-10-24 16:25:25 +0800

    feat: test

--- src/pages/mobile-exchange.js
+++ src/pages/mobile-exchange.js
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
-import { PendingOrders } from "../components/HistoryOrder";
-import MarketHistory from "../components/MarketHistory";
-import MarketTrade from "../components/MarketTrade";
-import DepthBook from "../components/DepthBook";
-import TradingChart from "../components/TradingChart";
 import { ThemeConsumer } from "../context/ThemeContext";
 import StoreContext from "../store/store-context";
 import { Tabs, Tab } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
-import MobileTickers from "../components/MobileTickers";
-import MobileTicker from "../components/MobileTicker";
-import DepthChart from "../components/DepthChart";
 
-const AccountMobileTile = React.lazy(() => import("../components/AccountMobileTile"));
+const TradingChart = React.lazy(() => import("../components/TradingChart"));
+const MobileTickers = React.lazy(() => import("../components/MobileTickers"));
+const MobileTicker = React.lazy(() => import("../components/MobileTicker"));
+const MarketTrade = React.lazy(() => import("../components/MarketTrade"));
+const DepthChart = React.lazy(() => import("../components/DepthChart"));
+const DepthBook = React.lazy(() => import("../components/DepthBook"));
+const PendingOrders = React.lazy(() => import("../components/PendingOrders"));
+const MarketHistory = React.lazy(() => import("../components/MarketHistory"));
+const AccountMobileTile = React.lazy(() =>
+  import("../components/AccountMobileTile")
+);
 
 const MobileExchange = (props) => {
   const storeCtx = useContext(StoreContext);
@@ -25,73 +27,77 @@ const MobileExchange = (props) => {
       //   storeCtx.setFocusEl(null);
       // }}
     >
-      {(storeCtx.activePage === "chart" ||
-        storeCtx.activePage === "market" ||
-        storeCtx.activePage === "trade") && <MobileTickers />}
-      {(storeCtx.activePage === "chart" ||
-        storeCtx.activePage === "market") && <MobileTicker />}
-      <section
-        className={`section${
-          storeCtx.activePage === "assets" ? " section--assets" : ""
-        }${storeCtx.activePage === "market" ? " section--market" : ""}`}
-      >
-        {storeCtx.activePage === "chart" && (
-          <>
-            <ThemeConsumer>
-              {({ data }) => <TradingChart theme={data.theme} />}
-            </ThemeConsumer>
-          </>
-        )}
-        {storeCtx.activePage === "market" && (
-          <>
-            <DepthChart />
-            <div className="order-book--mobile">
-              <DepthBook />
-            </div>
-          </>
-        )}
-        {storeCtx.activePage === "trade" && (
-          <>
-            <div className="section__container">
-              <MarketTrade />
-            </div>
-            <div className="section__container section__container--mobile">
-              <Tabs defaultActiveKey="market">
-                <Tab eventKey="market" title={t("navigator_market")}>
-                  <DepthBook />
-                </Tab>
-                {storeCtx.isLogin && (
-                  <Tab eventKey="my_orders" title={t("my_orders")}>
-                    <PendingOrders />
-                  </Tab>
+      <React.Suspense>
+        {(storeCtx.activePage === "chart" ||
+          storeCtx.activePage === "market" ||
+          storeCtx.activePage === "trade") && <MobileTickers />}
+        {(storeCtx.activePage === "chart" ||
+          storeCtx.activePage === "market") && <MobileTicker />}
+        <section
+          className={`section${
+            storeCtx.activePage === "assets" ? " section--assets" : ""
+          }${storeCtx.activePage === "market" ? " section--market" : ""}`}
+        >
+          {storeCtx.activePage === "chart" && (
+            <>
+              <ThemeConsumer>
+                {({ data }) => <TradingChart theme={data.theme} />}
+              </ThemeConsumer>
+            </>
+          )}
+          {storeCtx.activePage === "market" && (
+            <>
+              <DepthChart />
+              <div className="order-book--mobile">
+                <DepthBook />
+              </div>
+            </>
+          )}
+          {storeCtx.activePage === "trade" && (
+            <>
+              <div className="section__container">
+                <MarketTrade />
+              </div>
+              <div className="section__container section__container--mobile">
+                <Tabs defaultActiveKey="market">
+                  <React.Suspense>
+                    <Tab eventKey="market" title={t("navigator_market")}>
+                      <DepthBook />
+                    </Tab>
+                    {storeCtx.isLogin && (
+                      <Tab eventKey="my_orders" title={t("my_orders")}>
+                        <PendingOrders />
+                      </Tab>
+                    )}
+                    <Tab eventKey="trades" title={t("trades")}>
+                      <MarketHistory />
+                    </Tab>
+                  </React.Suspense>
+                </Tabs>
+              </div>
+            </>
+          )}
+          {storeCtx.activePage === "assets" && (
+            <React.Suspense>
+              <div className="mobole-account__list">
+                {storeCtx.accounts?.accounts ? (
+                  Object.values(storeCtx.accounts.accounts).map((account) => (
+                    <AccountMobileTile
+                      account={account}
+                      withTitle={false}
+                      showAvailable={true}
+                      showTotal={false}
+                    />
+                  ))
+                ) : (
+                  <div></div>
                 )}
-                <Tab eventKey="trades" title={t("trades")}>
-                  <MarketHistory />
-                </Tab>
-              </Tabs>
-            </div>
-          </>
-        )}
-        {storeCtx.activePage === "assets" && (
-          <React.Suspense>
-            <div className="mobole-account__list">
-              {storeCtx.accounts?.accounts ? (
-                Object.values(storeCtx.accounts.accounts).map((account) => (
-                  <AccountMobileTile
-                    account={account}
-                    withTitle={false}
-                    showAvailable={true}
-                    showTotal={false}
-                  />
-                ))
-              ) : (
-                <div></div>
-              )}
-            </div>
-          </React.Suspense>
-        )}
-        <div className="section__block"></div>
-      </section>
+              </div>
+            </React.Suspense>
+          )}
+          <div className="section__block"></div>
+        </section>
+      </React.Suspense>
     </main>
   );
 };
