4c6491a3de753b252a487d7a853fa9a3ec2bcff3
TzuHanLiang <crocodilestear@gmail.com>
2022-10-31 15:37:46 +0800

    feat: dbmigrate: outer_trade add column

--- dbmigrations/20221031001.sql
+++ dbmigrations/20221031001.sql
@@ -3,11 +3,11 @@ ADD COLUMN (
         `voucher_price` decimal (32, 16) DEFAULT NULL,
         `voucher_volume` decimal (32, 16) DEFAULT NULL,
         `voucher_fee` decimal (32, 16) DEFAULT NULL,
-        `voucher_fee_ccy` varchar(255) DEFAULT NULL,
-        `voucher_fee_ccy` varchar(255) DEFAULT NULL,
+        `voucher_fee_currency` varchar(255) DEFAULT NULL,
         `ask_account_version_id` int(11) DEFAULT NULL,
         `bid_account_version_id` int(11) DEFAULT NULL,
         `order_full_filled_account_version_id` int(11) DEFAULT NULL,
+        `referral_commission_id` int(11) DEFAULT NULL,
         `trade_counts` int(11) DEFAULT NULL,
         `done` int(11) DEFAULT NULL
     );
