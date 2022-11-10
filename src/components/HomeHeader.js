2f16dcd8bf7bdb52e782709741237d100680f0f8
TzuHanLiang <crocodilestear@gmail.com>
2022-11-10 16:43:01 +0800

    feat: add files

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,9 +3,30 @@ import StoreContext from "../store/store-context";
 
 const HomeHeader = () => {
   const storeCtx = useContext(StoreContext);
-  return <div className="home-header">
-    
-  </div>;
+  return (
+    <div className="home-header">
+      <a className="home-header__brand" href="/">
+        <img
+          src="/TideBit_White_hk.png"
+          className="d-inline-block align-top"
+          alt="TideBit"
+          width="175px"
+          height="60px"
+        />
+      </a>
+      <div className="home-header__collapse">
+        <div className="home-header__dropdown">
+          <input
+            className="home-header__dropdown--input"
+            type="radio"
+            name="home-header-dropdown"
+          />
+          <label className="home-header__dropdown--label"></label>
+          <div className="home-header__dropdown--options"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeHeader;
