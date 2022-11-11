e627b1246878d3fc4a30ddf2db17e3183be58c8a
TzuHanLiang <crocodilestear@gmail.com>
2022-11-11 18:04:35 +0800

    feat: add css & html

--- src/components/HomeStores.js
+++ src/components/HomeStores.js
@@ -6,24 +6,28 @@ const HomeStores = () => {
   return (
     <div className="home-stores">
       <div className="home-stores__container">
-        <a
-          className="home-stores__link"
-          href="https://itunes.apple.com/hk/app/tidebit/id1363945964?mt=8"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="home-stores__image"></div>
-        </a>
-        <a
-          className="home-stores__link"
-          href="https://play.google.com/store/apps/details?id=com.tideisun.tidebit"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="home-stores__image"></div>
-        </a>
+        <div className="home-stores__box">
+          <a
+            className="home-stores__link"
+            href="https://itunes.apple.com/hk/app/tidebit/id1363945964?mt=8"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="home-stores__image home-stores__image--apple"></div>
+          </a>
+          <a
+            className="home-stores__link"
+            href="https://play.google.com/store/apps/details?id=com.tideisun.tidebit"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="home-stores__image home-stores__image--google"></div>
+          </a>
+        </div>
+        <div className="home-stores__description">
+          {t("home_stores_description")}
+        </div>
       </div>
-      <div className="home-stores__description">{t("home_stores_description")}</div>
     </div>
   );
 };
