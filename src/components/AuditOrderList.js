a797f10e94f2c4125c48c71bf4ef23718bb1b79b
TzuHanLiang <crocodilestear@gmail.com>
2022-12-21 14:45:31 +0800

    feat: test

--- src/components/AuditOrderList.js
+++ src/components/AuditOrderList.js
@@ -2,7 +2,7 @@ import AuditOrder from "./AuditOrder";
 
 const AuditOrderList = (props) => {
   const component = props.orders?.map((order) => (
-    <AuditOrder key={`order-${order.id}`} order={order} />
+    <AuditOrder key={`order-${order.id}`} auditOrder={order} />
   ));
   return (
     <tbody className="screen__table-rows audit-order__list">{component}</tbody>
