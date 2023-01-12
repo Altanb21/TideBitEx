fa9fdc37d07057f4612c8f48f8830c3c6dfb4f2c
TzuHanLiang <crocodilestear@gmail.com>
2023-01-12 15:47:31 +0800

    feat: test

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -19,7 +19,7 @@ const countdown = (id, name) => {
     const timeslot = slots[timeslotIndex];
     clearTimeout(timeslot.timer);
     const elapsed = (now - timeslot.start) / 1000;
-    console.log(`name${name} elapsed ${elapsed}s`);
+    console.log(`name: ${name} elapsed: ${elapsed}s`);
     // delete timeslot
     slots.splice(timeslotIndex, 1);
   } else {
